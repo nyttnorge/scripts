82efb5e3b398b16cf51494eb47da1a73b611abc8
NyttNorge <nyttnorge@nyttnorge.com>
2017-09-27 20:00:30 +0200

    Calculating data

--- surface.rb
+++ surface.rb
@@ -1,14 +1,27 @@
 #!/usr/bin/env ruby
 
-x = ARGV[0].to_i
+# Earth radius in km
+EARTH_RADIUS = 6371.0
 
-x = x.to_f
+# Distance from Earth to Moon
+EARTH_TO_MOON_DISTANCE = 384400
 
-v = 3411.0
-r = 6371.0
+# Orbit total distance
+ORBIT_TOTAL_DISTANCE = EARTH_TO_MOON_DISTANCE * Math::PI * 2
 
-while x > 0
-  result = v / (Math.sqrt(1 - ((x / r)**2)))
-  puts "#{x}: #{result.round(12)}"
-  x -= 10
+# Moon orbital time in hours (period)
+MOON_ORBITAL_TIME = 29.53 * 24
+
+# Moon average orbital speed
+MOON_ORBITAL_SPEED = ORBIT_TOTAL_DISTANCE / MOON_ORBITAL_TIME
+
+puts "Using moon orbital speed of #{MOON_ORBITAL_SPEED.round(2)} km/h"
+
+# Distance from center
+distance = ARGV[0].to_f
+
+while distance > 0
+  result = MOON_ORBITAL_SPEED / (Math.sqrt(1 - ((distance / EARTH_RADIUS)**2)))
+  puts "#{distance}: #{result.round(12)}"
+  distance -= 10
 end
